d9924a995b33d101cad2485fbd14d1264d3feaca
Muhammad Junaid <rjunaid7814@gmail.com>
2026-03-10 12:18:49 +0500

    Add Streamlit app code

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+import numpy as np
+from tensorflow.keras.models import load_model
+from PIL import Image
+
+model = load_model("mnist_model.h5")
+
+st.title("MNIST Digit Recognition")
+
+canvas = st_canvas(
+    fill_color="black",
+    stroke_width=15,
+    stroke_color="white",
+    background_color="black",
+    height=280,
+    width=280,
+    drawing_mode="freedraw",
+    key="canvas"
+)
+
+if canvas.image_data is not None:
+
+    img = Image.fromarray(canvas.image_data.astype('uint8')).convert('L')
+    img = img.resize((28,28))
+
+    img = np.array(img)
+    img = 255 - img
+    img = img / 255.0
+    img = img.reshape(1,28,28,1)
+
+    if st.button("Predict"):
+
+        prediction = model.predict(img)
+        digit = np.argmax(prediction)
+
+        st.success(f"Predicted Digit: {digit}")
