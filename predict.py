acc2da14b7ffaba616028e9f870f87906c1cfdd8
Muhammad Junaid <rjunaid7814@gmail.com>
2026-03-11 20:42:23 +0500

    Update predict_digit function for image preprocessing
    Refactor image preprocessing in predict_digit function to invert pixel values before normalization.

--- predict.py
+++ predict.py
@@ -5,10 +5,18 @@ from PIL import Image
 model = load_model("mnist_model.h5")
 
 def predict_digit(img):
-    img = img.resize((28,28)).convert('L')
+
+    img = img.convert("L")
+    img = img.resize((28,28))
+
     img = np.array(img)
-    img = img/255.0
+
+    img = 255 - img
+
+    img = img / 255.0
+
     img = img.reshape(1,28,28,1)
 
     prediction = model.predict(img)
+
     return np.argmax(prediction)
